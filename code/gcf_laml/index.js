b43c28a619ea3ec47c73801f822fd10550113953
Luca Pradovera <luca.pradovera@gmail.com>
2020-09-14 11:51:40 +0200

    Simple GCF example

--- code/gcf_laml/index.js
+++ code/gcf_laml/index.js
@@ -2,9 +2,23 @@ const { RestClient } = require('@signalwire/node')
 
 exports.helloLaml = (req, res) => {
   const response = new RestClient.LaML.VoiceResponse();
-  response.say('Welcome to SignalWire on Google Cloud Functions.');
+  try {
+    var from_num = req.body.From.split('@')[0].replace('sip:', '');
+    
 
-  res.status(200)
-    .set('Content-Type', 'text/xml')
-    .send(response.toString());
+    if (from_num.match(/\+\d{11}/)) {
+      dial = response.dial({timeout: 30, callerID: from_num});
+      dial.number(req.body.SipUser);
+    } else {
+      response.hangup
+    }
+
+    console.log(response.toString());
+  } catch(err) {
+    console.log(err.message);
+  } finally {
+    res.status(200)
+      .set('Content-Type', 'text/xml')
+      .send(response.toString());
+  }
 };
