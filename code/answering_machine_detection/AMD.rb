b5531dc5e5018f011306bb08beb8aca8b48528fb
Luca Pradovera <luca.pradovera@gmail.com>
2021-03-16 11:56:26 +0100

    Clean up and test

--- code/answering_machine_detection/AMD.rb
+++ code/answering_machine_detection/AMD.rb
@@ -2,48 +2,28 @@ require 'rubygems'
 require 'sinatra'
 require 'signalwire'
 
-def giveResponse
-     puts "Generating LaML bin here"
-     # replace messsage with whatever voicemail you want to leave 
-     response = Signalwire::Sdk::VoiceResponse.new do |response|
-        response.say(message: "I am honored to be with you today at your commencement from one of the finest universities in
-            the world. I never graduated from college. Truth be told, this is the closest I've ever gotten to a
-            college graduation. Today I want to tell you three stories from my life. That's it. No big deal. Just
-            three stories... When I was young, there was an amazing publication called The Whole Earth
-            Catalog, which was one of the bibles of my generation. It was created by a fellow named
-            Stewart Brand not far from here in Menlo Park, and he brought it to life with his poetic touch.
-            This was in the late 1960's, before personal computers and desktop publishing, so it was all
-            made with typewriters, scissors, and polaroid cameras. It was sort of like Google in paperback
-            form, 35 years before Google came along: it was idealistic, and overflowing with neat tools and 
-            great notions.
-            Stewart and his team put out several issues of The Whole Earth Catalog, and then when it had
-            run its course, they put out a final issue. It was the mid-1970s, and I was your age. On the back
-            cover of their final issue was a photograph of an early morning country road, the kind you might
-            find yourself hitchhiking on if you were so adventurous. Beneath it were the words: Stay
-            Hungry. Stay Foolish. It was their farewell message as they signed off. Stay Hungry. Stay
-            Foolish. And I have always wished that for myself. And now, as you graduate to begin anew, I
-            wish that for you.
-            Stay Hungry. Stay Foolish.")
-      end
-    response.to_s
-end
+set :port, 8080
 
-def checkIfHuman
-    case params[:AnsweredBy]
-    when 'machine_end_beep'
-        giveResponse
-    when 'machine_end_other'
-        giveResponse
-    when 'machine_end_silence'
-        giveResponse
-    when 'human'
-        'use regular code to redirect to normal call flow here'
-    end
-end
 post '/start' do
-    content_type 'text/xml'
-    puts "*******************params***************************"
-    puts params
-    puts "*******************params***************************"
-    checkIfHuman
+  response = Signalwire::Sdk::VoiceResponse.new
+
+  case params[:AnsweredBy]
+  when 'machine_end_other', 'machine_end_silence', 'machine_end_beep'
+    puts "It's a machine!"
+    # put in a little pause to allow the recording to start on the other end
+    response.pause(length: 1)
+    # replace messsage with whatever voicemail you want to leave
+    response.say(message: "Hello! This is the County Medical Center. We are calling you to confirm your doctor appointment. Please call us back as soon as possible.")
+    response.hangup
+  when 'human'
+    puts "We got ourselves a live human here!"
+    response.dial do |dial|
+      # defaulting to calling a time voice announcement number as an example
+      dial.number(ENV.fetch('AGENT_NUMBER', '+12027621401'))
+    end
+  end
+
+  # debug the LAML
+  puts response.to_s
+  response.to_s
 end
--- code/answering_machine_detection/README.md
+++ code/answering_machine_detection/README.md
@@ -1,9 +1,33 @@
-# Answering Machine Detection (for guide but most importantly for Plappert case 00016066)
+# Answering Machine Detection
 
-This is the code that I've gotten so far, and it works but not 100% correctly. The <Say> starts before the voicemail beep plays, so the recording is a few sentences too late. I don't want to share with Plappert until we can make sure that the full message plays for him, and same for creating the guide of course. Hoping that you are able to figure it out and thanks for your help!
-  
-# Example Call with associated voicemail 
-https://internal.signalwire.com/tools/inspect_objects?utf8=%E2%9C%93&search%5Bobject_type%5D=laml_call&search%5Bterm%5D=161d9e20-f30d-4baf-b8ca-b7912dfeaacf&commit=Search
+[write up goes here]
+## Triggering calls
 
-![image](https://user-images.githubusercontent.com/58792902/111007184-dee4e880-8353-11eb-97ca-7c909c07ef0d.png)
+```ruby
+client = Signalwire::REST::Client.new ENV['SIGNALWIRE_PROJECT_KEY'], ENV['SIGNALWIRE_TOKEN'], signalwire_space_url: ENV['SIGNALWIRE_SPACE']
 
+  call = client.calls.create(
+    url: ENV['APP_DOMAIN'] + '/start',
+    to: params[:to],
+    from: ENV['FROM_NUMBER'],
+    machine_detection: "DetectMessageEnd"
+  )
+```
+
+```bash
+curl https://YOURSPACE.signalwire.com/api/laml/2010-04-01/Accounts/YOUR-PROJECT-ID/Calls.json \
+  -X POST \
+  --data-urlencode "Url=YOUR-WEBHOOK-URL" \
+  --data-urlencode "To=DESTINATION-NUMBER" \
+  --data-urlencode "From=CALLER-ID-FROM-SIGNALWIRE-ACCOUNT" \
+  --data-urlencode 'MachineDetection=DetectMessageEnd' \
+  -u "YOUR-PROJECT-ID:YOUR-TOKEN"
+```
+
+## Docker instructions
+
+Copy `env.example` to `.env` and edit.
+
+Build with `docker build . -t rubyamd`
+
+Run with `docker run -it --rm -p 8080:8080 --name rubyamd --env-file .env rubyamd`
