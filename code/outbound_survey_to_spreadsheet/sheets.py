b4187ac2edbe7611547516e41a3886ac9b73528f
cassieemb <58792902+cassieemb@users.noreply.github.com>
2021-04-29 13:58:04 -0500

    JSON IVR Example

--- code/outbound_survey_to_spreadsheet/sheets.py
+++
@@ -1,8 +0,0 @@
-from oauth2client.service_account import ServiceAccountCredentials
-import gspread
-import os
-
-scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name(os.getcwd() + "/creds.json", scope)
-client = gspread.authorize(creds)
-sheet = client.open("Covid_Survey").sheet1
