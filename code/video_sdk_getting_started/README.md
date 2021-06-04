42d13e00a5d436fb1e1f0661fe544c643caf3be1
Nirav Nikhil <niravnikhil@gmail.com>
2021-06-05 00:40:38 +0545

    Add Video SDK getting started sample code (#44)

---
+++ code/video_sdk_getting_started/README.md
@@ -0,0 +1,39 @@
+# SignalWire Video SDK Getting Started Sample Application
+
+This example uses the [SignalWire Video API](https://docs.signalwire.com/topics/api/video) and the [SignalWire Video SDK for Javascript](https://docs.signalwire.com/topics/api/sdk/js) to
+create a simple video conferencing software.
+
+## Prerequisites
+
+You will need a SignalWire account, and you can sign up [here](https://m.signalwire.com/signups/new?s=1). From your SignalWire account, you'll need to note your Project ID, your Space URL and
+an API token which you can generate from the API tab in your dashboard.
+
+If you are looking for more information about using SignalWire, refer to our [Getting Started](https://signalwire.com/resources/getting-started/signalwire-101) guide.
+
+## Configuration
+
+To store your SignalWire API credentials, you need to create a file named `.env` at the root of the directory (next to `package.json`). The three project variables we previously noted will be placed there in the following format.
+
+```
+PROJECT_ID=<Project ID here>
+API_TOKEN=<Your API token here>
+API_URL=https://<your space name>.signalwire.com/api/video
+```
+
+## Running the application
+
+If you are running the application locally, simply create an `.env` file and set it up as above. Then start the application with `node index.js`.
+
+It is simpler to run the application via Docker, **please give the Dockerfile a look, Luca**.
+
+Either way, after starting the application, head to `http://localhost:4000` to see a test page.
+
+## Get started with SignalWire
+
+If you would like to test this example out, you can create a SignalWire account and space [here](https://m.signalwire.com/signups/new?s=1).
+
+Your account will be made in trial mode, which you can exit by making a manual top up of $5.00. You can find more information [on the Trial Mode resource page](https://signalwire.com/resources/getting-started/trial-mode).
+
+If you are looking for more information about using SignalWire, refer to our [Getting Started](https://signalwire.com/resources/getting-started/signalwire-101) guide.
+
+Please feel free to reach out to us on our Community Slack or create a Support ticket if you need guidance!
---
+++ code/video_sdk_getting_started/index.js
@@ -0,0 +1,86 @@
+require('dotenv').config()
+
+const auth = {
+    username: process.env.PROJECT_ID, // Project-ID
+    password: process.env.API_TOKEN, // API token 
+};
+const apiurl = process.env.API_URL;
+const ROOMNAME = "testroom";
+
+// Basic express boilerplate
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const axios = require('axios')
+
+const app = express();
+app.use(bodyParser.json())
+app.use(cors());
+// End basic express boilerplate
+
+async function CreateRoomIfItDoesntExist(name) {
+    console.log("Creating room " + name + " if it doesn't exist.")
+    let existingrooms = []
+    let room_name = name.toLowerCase();
+    try {
+        // Get the list of rooms
+        console.log(" - Getting the list of rooms")
+        let rooms = await axios.get(apiurl + "/rooms", { auth });
+        existingrooms = rooms.data.rooms.map(x => x.name.toLowerCase())
+        console.log(" - Rooms that currently exist: ", existingrooms)
+    }
+    catch (e) {
+        console.log(e)
+        return false;
+    }
+
+    try {
+        // Create a new room if the room doesn't yet exist
+        if (!existingrooms.includes(room_name)) {
+            console.log(" - Room " + name + " didn't exist. Trying to create new room.")
+            await axios.post(apiurl + "/rooms", { name: room_name }, { auth })
+            console.log(" - New room created")
+        }
+        else {
+            console.log(" - Room already existed. Doing nothing.")
+        }
+    }
+    catch (e) {
+        console.log(e);
+        return false
+    }
+    return true;
+}
+
+
+// Endpoint to request token for video call
+app.post('/get_token', async (req, res) => {
+    let { user_name } = req.body;
+    console.log("Received name", user_name)
+    try {
+        let token = await axios.post(
+            apiurl + "/room_tokens",
+            { user_name, room_name: ROOMNAME },
+            { auth }
+        );
+        console.log(token.data.token)
+        return res.json({ token: token.data.token })
+    } catch (e) {
+        console.log(e);
+        return res.sendStatus(500);
+    }
+})
+
+app.use('/', express.static("frontend"))
+
+
+async function start(port) {
+    if (await CreateRoomIfItDoesntExist(ROOMNAME))
+        app.listen(port, () => {
+            console.log("Server listening at port", port)
+        });
+    else console.log("Couldn't create room. Quitting.")
+}
+
+// Start the server
+start(4000);
